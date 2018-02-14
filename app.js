45ba925e107800061141153fec3f7cf774e7eadb
ByronGBP <byrongbp@gmail.com>
2018-02-14 13:24:32 +0100

    add .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -13,7 +14,7 @@ const users = require('./routes/user');
 const app = express();
 
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/deploy-test', {
+mongoose.connect(process.env.MONGODB_URI, {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE
 });
