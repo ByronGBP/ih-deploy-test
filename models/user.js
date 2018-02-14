0cb161f6dc5f43563c6b64d13e3e51893e2f7729
ByronGBP <byrongbp@gmail.com>
2018-02-14 12:40:05 +0100

    setup users

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  name: String,
+  age: String
 }, {
     timestamps: {
       createdAt: "created_at",
--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,30 @@ const User = require('../models/user');
 /* GET home page. */
 router.get('/', (req, res, next) => {
 
+  User.find({}, (err, result) => {
+    if (err) {
+      next(err);
+    }
+    const data = {
+      users: result
+    }
+    res.render('user', data);
+  });
+});
+
+router.post('/', (req, res, next) => {
   const data = {
-    users: [{name: "", age: ""}]
+     name: req.body.username,
+     age: req.body.age
   }
-  res.render('user', data);
+
+  const newUser = new User(data);
+  newUser.save((err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/users');
+  })
 });
 
 module.exports = router;
